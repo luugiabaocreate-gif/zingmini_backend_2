4186153d74add5e8a3d589df5e54234e666deecb
luugiabaocreate-gif <luugiabao.create@gmail.com>
2025-10-25 09:12:10 +0700

    update backend code

--- models/Short.js
+++ models/Short.js
@@ -1,10 +1,14 @@
 import mongoose from "mongoose";
 
 const shortSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: false,
+  }, // ✅ cho phép trống
   videoUrl: { type: String, required: true },
   description: { type: String },
-  createdAt: { type: Date, default: Date.now }
+  createdAt: { type: Date, default: Date.now },
 });
 
 export default mongoose.model("Short", shortSchema);
