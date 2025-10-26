f10138c7becacfd849d986f2f3328a6b61d71baa
luugiabaocreate-gif <luugiabao.create@gmail.com>
2025-10-26 08:52:10 +0700

    update backend code

--- routes/stories.js
+++ routes/stories.js
@@ -32,9 +32,9 @@ const upload = multer({ storage }); */
 
 // cấu hình cloudinary (dùng env vars)
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUD_CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
